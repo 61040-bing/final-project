ca56b1c130da7d73715015c0ca87441f7d1ae32e
Naomi Kirimi <nkirimi@mit.edu>
2022-12-04 21:03:20 -0500

    roundtable shows username correctly

--- client/components/roundtable/RoundTableComponent.vue
+++ client/components/roundtable/RoundTableComponent.vue
@@ -7,10 +7,10 @@
   >
     <header>
       <h3 class="author">
-        @{{ roundtable.author }}
+        @{{ roundtable.author.firstName + " " + roundtable.author.lastName }}
       </h3>
       <div
-        v-if="$store.state.username === roundtable.author"
+        v-if="$store.state. userObject === roundtable.author"
         class="actions"
       >
         <button @click="deleteRoundTable">
