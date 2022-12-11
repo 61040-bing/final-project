95bd722d028fc8b531391c0d2403d47b5710d615
Brian Ntanga <>
2022-12-10 22:50:14 -0500

    resetting flag

--- client/components/Login/RegisterForm.vue
+++ client/components/Login/RegisterForm.vue
@@ -164,7 +164,7 @@ export default {
   data() {
     return {
       alerts: {},
-      selectingNeighborhood: true,
+      selectingNeighborhood: false,
       url: '/api/users',
       method: 'POST',
       hasBody: true,
